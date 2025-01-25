4da01e2a7d28946681433e5735a26a75b89d546c
persabellasirak <persabella.sirak@gmail.com>
2025-01-24 20:37:59 -0600

    Update Problem5.java

--- src/Problem5.java
+++ src/Problem5.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Problem5 {
-}
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3, 4, 5, 3};
+        int val = 3;
+        int k = removeElement(nums, val);
+
+        System.out.println("Number of elements not equal to " + val + ": " + k);
+        System.out.println("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
